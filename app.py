c0c943a989d7a488c654aa2c663388d5c82b153f
kr <krrishikarana83@gmail.com>
2025-08-05 10:34:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+st.set_page_config(page_title="Lottie Demo", page_icon="✨", layout="wide")
+
+st.title("Lottie Animation Example")
+lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
+lottie_json = load_lottieurl(lottie_url)
+
+st_lottie(lottie_json, height=300, key="coding")
