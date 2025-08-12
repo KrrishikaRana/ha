e098f59052a52e02e2c17788729f04567ec9b160
kr <krrishikarana83@gmail.com>
2025-08-12 10:14:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ st.markdown(
 # Mood animation centered
 col1, col2, col3 = st.columns([1, 3, 1])
 with col2:
-    st_lottie(mood_anim, height=600, key="center_anim")
+    st_lottie(mood_anim, height=800, key="center_anim")
